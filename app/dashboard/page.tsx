ad4cfcecb0733d5000fcc56d9a50c7d61b3885e1
Lhawhalhe001 <onabiyiabdulsamod@gmail.com>
2025-10-27 21:36:32 +0100

    The dashboard make we start

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div className='flex justify-center h-screen w-screen items-center flex-col text-center'>
-        <h1 className='font-semibold text-base text-2xl'>ECOVEST</h1>
-        This is the app.
+    <div className="flex justify-center h-screen w-screen items-center flex-col text-center">
+      <h1 className="font-semibold text-base text-2xl">ECOVEST</h1>
+      This is the app.
+      <h1>Lets continue this shii guy</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
