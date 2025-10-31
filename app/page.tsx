545010815d9a6c21e75d1598a90e8e0888af5a6e
therealteejay25 <therealteejay25@gmail.com>
2025-10-31 18:21:39 +0100

    router method push

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,14 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="relative w-screen h-screen">
-      {/* Fullscreen background image */}
-      <Image
-        src="/logo.png"
-        alt="Background"
-        fill
-        className="object-cover"
-        priority
-      />
+  const router = useRouter();
 
-      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/40 space-y-6">
-        <h1 className="text-white text-8xl font-bold tracking-wide">ECOVEST</h1>
+  useEffect(() => {
+    router.push("/auth/login");
+  }, [router]);
 
-        <Link href="/auth/login">
-          <button className="bg-base text-white py-4 px-7 font-semibold hover:bg-green-800 transition cursor-pointer text-xl">
-            Start investing
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
+  return <div className="relative w-screen h-screen"></div>;
 }
