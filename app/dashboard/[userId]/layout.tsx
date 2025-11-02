e3d35fb596290deb9d1183aba4d06a9858cf2a65
Xprogrammer123 <fawassaka862@gmail.com>
2025-11-03 00:44:11 +0100

    commit

--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -12,7 +12,9 @@ export default function DashboardLayout({
     <div className="flex flex-col md:flex-row min-h-screen bg-[#f8f9f8]">
       <Sidebar />
 
-      <main className="flex-1 p-4 md:p-6 overflow-y-auto">{children}</main>
+      <main className="flex-1 p-4 md:p-6 overflow-y-auto md:ml-[22rem]">
+        {children}
+      </main>
     </div>
   );
 }
