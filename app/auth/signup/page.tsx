4df010e3c818f5d2ca1850e5ed1d371efe937d7a
Testicode234 <xprogrammerr@gmail.com>
2025-11-02 19:36:09 +0100

    register route issue fixed

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -37,7 +37,7 @@ const Page = () => {
       }
 
       console.log("User registered:", data.user);
-      window.location.href = `/login`;
+      window.location.href = `/auth/login`;
     } catch (err) {
       console.error(err);
       setError("Something went wrong. Please try again later.");
