f055d02dd4a70ce01e8da099b23694e9883fc1a4
Xprogrammer123 <fawassaka862@gmail.com>
2025-11-10 23:14:02 +0100

    commit

--- app/dashboard/[userId]/profile/page.tsx
+++ app/dashboard/[userId]/profile/page.tsx
@@ -3,7 +3,7 @@
 export default function ProfilePage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Profile Overview</h1>
+      <h1 className="text-2xl font-bold mb-4">PROFILE OVERVIEW</h1>
     </div>
   );
 }
