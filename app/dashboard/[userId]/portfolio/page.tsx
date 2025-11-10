c149b3c68152eb36730e26eb31626c004f9dfb4e
Xprogrammer123 <fawassaka862@gmail.com>
2025-11-10 23:15:19 +0100

    commit

--- app/dashboard/[userId]/portfolio/page.tsx
+++ app/dashboard/[userId]/portfolio/page.tsx
@@ -3,7 +3,7 @@
 export default function PortfolioPage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">PORTFOLIO OVERVIEW</h1>
+      <h1 className="text-2xl font-bold mb-4">Portfolio Overview</h1>
     </div>
   );
 }
