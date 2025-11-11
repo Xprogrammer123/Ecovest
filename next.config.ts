c3b435ab280c483e4902017fed7644b3a7692e85
Xprogrammer123 <fawassaka862@gmail.com>
2025-11-11 21:02:02 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://ecovest-api.onrender.com/:path*", 
-      },
-    ];
-  },
+ 
+  
 };
 
 export default nextConfig;
