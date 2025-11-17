89404478d6e400c78fcda836e49c29d96ad621fd
Xprogrammer123 <fawassaka862@gmail.com>
2025-11-17 06:45:23 +0100

    commit

--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
           {children}
 
 
-          <AssistantModal />
+        
         </main>
       </div>
     </AssistantProvider>
