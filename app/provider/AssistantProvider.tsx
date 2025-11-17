502d9923126bdf9dc3a699d702941f9cf87e850d
Xprogrammer123 <fawassaka862@gmail.com>
2025-11-17 06:21:26 +0100

    Maybe final push

--- app/provider/AssistantProvider.tsx
+++ app/provider/AssistantProvider.tsx
@@ -3,8 +3,10 @@
 import React from "react";
 import { AssistantRuntimeProvider, useAssistantRuntime } from "@assistant-ui/react";
 
-const runtime = useAssistantRuntime();
 
 export function AssistantProvider({ children }: { children: React.ReactNode }) {
+
+const runtime = useAssistantRuntime();
+
   return <AssistantRuntimeProvider runtime={runtime}>{children}</AssistantRuntimeProvider>;
 }
